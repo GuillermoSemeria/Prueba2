22c64f0a079acee74b092522aee9915ee96330a9
GuillermoSemeria <18170452@itculiacan.edu.mx>
2023-03-29 22:11:55 -0700

    conexion base de datos

--- Index.js
+++ Index.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'Carrasos'
+  database: 'libros'
 });
 
 connection.connect((error) => {
